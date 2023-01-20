8997ecaa226c8e649c3be3c790b50a20c96b734d
shankleshdivami <shanklesh@divami.com>
2023-01-20 18:13:05 +0530

    fff

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import app from "./app.js";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
+  console.log("======================");
   console.log("Example app listening on port 3000!");
 });
