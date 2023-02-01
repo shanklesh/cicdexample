c1d09926f84154db6228156e5dcd23f0e22677ff
shankleshdivami <shanklesh@divami.com>
2023-02-01 22:09:43 +0530

    sudo added

--- deploy_to_ec2.sh
+++ deploy_to_ec2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo files are uploading:
-scp -i ./shanklesh.pem ./shanklesh.txt  ubuntu@ec2-43-207-55-237.ap-northeast-1.compute.amazonaws.com:~/
+sudo scp -i ./shanklesh.pem ./shanklesh.txt  ubuntu@ec2-43-207-55-237.ap-northeast-1.compute.amazonaws.com:~/
 echo uploaded successfully:
